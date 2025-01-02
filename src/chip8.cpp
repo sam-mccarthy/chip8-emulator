81a4bacd7061335bae74d8035f67cae2358f20f7
sam-mccarthy <samuelmccarthy03@gmail.com>
2025-01-02 03:34:46 -0500

    File loading implemented

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,21 +1,34 @@
 #include "chip8.h"
-
-Chip8::Chip8() {
-
-}
+#include "system.h"
+#include <iostream>
+#include <fstream>
 
 Chip8::Chip8(const std::string &filename) {
-
+    _program = nullptr;
+    _program_size = 0;
+    load_file(filename);
 }
 
 Chip8::~Chip8() {
-
+    delete _program;
 }
 
 void Chip8::load_file(const std::string &filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if(file.is_open()){
+        file.seekg(0, std::ios_base::end);
+        // The memory of the emulator is limited to 0x1000, so
+        // we'll only allow files up to that limit, excluding fonts.
+        _program_size = std::min(0x1000 - 0x200, (int)file.tellg());
+        file.seekg(0, std::ios_base::beg);
 
+        _program = new uint8_t[_program_size];
+        file.read((char*)_program, _program_size);
+    } else {
+        std::cerr << "Error opening file.";
+    }
 }
 
 void Chip8::run() {
-
+    System system(_program, _program_size);
 }
--- src/chip8.h
+++ src/chip8.h
@@ -4,11 +4,14 @@
 
 class Chip8 {
 public:
-    Chip8();
+    Chip8() = default;
     Chip8(const std::string& filename);
 
     ~Chip8();
 
     void load_file(const std::string& filename);
     void run();
+private:
+    uint8_t* _program;
+    size_t _program_size;
 };
